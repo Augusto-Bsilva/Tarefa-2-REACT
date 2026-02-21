c659a4ef488b6fe9013535bd984b3cb5aebabb43
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-21 14:02:20 -0300

    feat: implementando rotas

---
+++ src/router.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from './pages/Login';
+/*import Home from './pages/Home';
+import Book from './pages/Book';
+import VerMais from './pages/VerMais'
+import RootLayout from "./layouts/RootLayout";*/
+
+const router = createBrowserRouter([
+    
+    {
+        
+        path:"/",
+        element:<Login />
+    
+    },
+    /*{
+        path: "/home",
+        element: <RootLayout />,
+        children: [
+            {
+                index:true,
+                element: <Home />
+
+            },
+            {
+                path: "/book/:bookId",
+                element: <Book />
+
+            },
+            {
+                path: "/vermais",
+                element: <VerMais />
+
+
+            },
+        ]
+    }*/
+    
+])
+
+export default router
