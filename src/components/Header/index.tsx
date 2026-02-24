b87764130b389aae641cf7fffea4ba6bb2242843
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-24 18:26:11 -0300

    feat: estilizando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,25 @@
+import style from './styles.module.css'
+import Logo from '../../assets/Logo.png'
+import Profile from '../../assets/Profile.png'
+import Cart from '../../assets/Shop.png'
+import { useNavigate } from 'react-router-dom'
+
 export default function Header(){
+    const navigate = useNavigate()
+    function handleClick(){
+        navigate('/')
+    }
     return(
-        <header><h1>titulooooooooo</h1></header>
+        <header className={style.menu}>
+            <img src={Logo} className={style.logo}/>
+            <div className={style.buttons}>
+                <button className={style.button} onClick={handleClick}>
+                    <img src={Profile} className={style.pic} />
+                </button>
+                <button className={style.button}>
+                    <img src={Cart} className={style.pic}/>
+                </button>
+            </div>
+        </header>
     )
 }
