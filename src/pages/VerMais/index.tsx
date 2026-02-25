e80a97c992d9e030467011313e0542edc036b035
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-25 13:16:26 -0300

    feat: implmentando página "ver mais"

--- src/pages/VerMais/index.tsx
+++ src/pages/VerMais/index.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import type bookProps from "../../types/Livros";
+import axios from "axios";
+import FileiraGenero from "../../components/Fileira/fileira";
+import style from './styles.module.css'
+import { useParams } from "react-router-dom";
+import Card from "../../components/Card/Card";
+
 export default function VerMais(){
-    return(
-        <section>
-        <h1>helooooooo</h1>
+    const [livros,setLivros] = useState<bookProps[]>([])
+    const { genero } = useParams()
+    
+    useEffect(()=>{
+            const config={
+                params:{
+                genero:genero
+     
+                }
+             }
+             axios.get('http://localhost:3000/livros',config)
+             .then((response)=> setLivros(response.data))
+             .catch((erro)=>{console.error('Erro na requisição',erro);})
+         },[])
+    
+         return(
+        <section className={style.lines}> 
+            {livros.map((livro)=>(
+                <Card key={livro.id} {...livro} />
+            ))}
         </section>
     )
 }
