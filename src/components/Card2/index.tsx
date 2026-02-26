883fd030fe5522aa00dad4d88a7b604245bb597c
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-26 11:31:29 -0300

    feat: implementando página do livro individual

--- src/components/Card2/index.tsx
+++ src/components/Card2/index.tsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import type bookProps from "../../types/Livros";
 import style from './styles.module.css'
 
 export default function Card ({ id,titulo ,autor,genero,preco,sinopse,capa }:bookProps){
+    
+    const navigate = useNavigate()
+    function handleClick(){
+        navigate(`book/${id}`)
+    }
+    
+    
     return(
         <div>
-            <button className={style.bookCard}>
+            <button className={style.bookCard} onClick={handleClick}>
                 <img src={capa} className={style.cover}/>
                 <div className={style.data}>
                 <div className={style.info}>
--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,14 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import type bookProps from "../../types/Livros"
 
 export default function Book(){
-    //const { bookId } = useParams()
-    //const book = livros.find(book => book.id === Number(bookId))
-    //if(!book){
-    //    return <h1>Livro não encontrado</h1>
-    //}
+    const [books,setBooks] = useState<bookProps[]>([])
+    const { bookId } = useParams()
+    
+    useEffect(()=>{
+             axios.get('http://localhost:3000/livros')
+             .then((response)=> setBooks(response.data))
+             .catch((erro)=>{console.error('Erro na requisição',erro);})
+        },[])
+        
+    const book = books.find(book => book.id === Number(bookId))
+     if(!book){
+        return <h1> Livro não encontrado!</h1>
+    }
     return(
-        <section><h1>King</h1>
-            <p>Reinadoooooooooooo</p>
+        <section>
+            <h1>Nome do livro: {book.titulo}</h1>
+            <p>preco do livro: </p>
+           
         </section>
            
         
