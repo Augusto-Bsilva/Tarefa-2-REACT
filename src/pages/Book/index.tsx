cdbc20c39bc092c7ec913cdf08d5de5db5ae8845
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-26 15:26:04 -0300

    feat: estilizando página do livro individual

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,16 +1,48 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useLoaderData, useParams } from "react-router-dom"
+
+import { useLoaderData, useNavigate } from "react-router-dom"
 import type bookProps from "../../types/Livros"
+import Arrow from '../../assets/Arrow.png'
+import style from './styles.module.css'
 
 export default function Book(){
     const book: bookProps = useLoaderData()
-
+    const navigate = useNavigate();
+    function handleClick(){
+        navigate(`/home/vermais/${book.genero}`)
+    }
   
     return(
         <section>
-            <h1>Nome do livro: {book.titulo}</h1>
-            <p>preco do livro: </p>
+            
+             <div className={style.title}>
+                <button 
+                    onClick={handleClick}>
+                    <img src={Arrow} 
+                    className={style.back}/>
+                </button>
+                <h1>Detalhes do livro</h1>
+            </div>
+            
+            <div className={style.sumary}>
+                
+                <div className={style.card}>
+                  <img src={book.capa}/>
+                </div>
+                
+                <div className={style.more}>
+                    
+                    <div className={style.names}>
+                        <h1>{book.titulo}</h1>
+                        <h2>{book.autor}</h2>
+                    </div>
+                    <div className={style.sinopse}>
+                        <h1>Sinopse</h1>
+                        <p>{book.sinopse}</p>
+                    </div>
+                
+                </div>
+            
+            </div>
            
         </section>
            
