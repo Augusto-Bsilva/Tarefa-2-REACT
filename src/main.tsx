215915c835b425c0b709088936060d7e8fdfe70a
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-27 17:36:53 -0300

    feat: implmentando adaptdores para requisições axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import './reset.css'
 import App from './App.tsx'
+import { QueryProvider } from './providers/query.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <QueryProvider>
+        <App />
+    </QueryProvider>
 )
